fc55b965a0b9746a80dcad88bf06d3d736f2b59b
JuliaGrisales <jgrisales@gmail.com>
2020-04-01 19:09:16 +0200

    Inicializa la base de datos con datos de prueba

--- ContosoUniversity/Models/Course.cs
+++ ContosoUniversity/Models/Course.cs
@@ -11,7 +11,7 @@ namespace ContosoUniversity.Models
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int CourseID { get; set; }
         public string Title { get; set; }
-        public string Credits { get; set; }
+        public int Credits { get; set; }
 
         public virtual ICollection<Enrollment> Enrollments { get; set; }
     }
